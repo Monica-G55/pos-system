6f3b086b3a5e92bdbf6804410155d11e52fb5338
Monica-G55 <monics5553@gmail.com>
2026-06-30 13:31:01 +0530

    changes

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -81,9 +81,9 @@
              </div>
 
          </div>
-         <div class="sb-sidenav-footer">
+         <!-- <div class="sb-sidenav-footer">
              <div class="small">Logged in as:</div>
              Start Bootstrap
-         </div>
+         </div> -->
      </nav>
  </div>
